3ebea6202a171b5f5ef209f54bfaf6cf005ae4ad
Daniel Morales <155024431+speedbump5421@users.noreply.github.com>
2024-12-12 10:04:48 -0600

    Add files via upload

---
+++ test_classes.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+def test_classes_compare():
+    p1 = Person("Mindy")
+    p2 = Person("Daniel")
+    assert p1.name == p2.name
